0f043593b5f960d05ed0274b8ae1124c5522c061
justinc987 <justinc9877@gmail.com>
2019-04-23 08:48:09 -0400

    created header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,9 +26,10 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-	declarations: [ AppComponent, HomeComponent ],
+	declarations: [ AppComponent, HomeComponent, HeaderComponent ],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
