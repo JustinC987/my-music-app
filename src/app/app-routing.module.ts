c4039b6d2acd92b020d2f73e33e0be34416fd82a
justinc987 <justinc9877@gmail.com>
2019-04-26 17:52:40 -0400

    Added sign-up route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 const routes: Routes = [
+	{
+		path: 'sign-up',
+		component: SignUpComponent
+	},
 	{
 		path: '',
 		component: HomeComponent
